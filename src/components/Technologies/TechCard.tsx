24196fc36fa4ef6fc430b4273731d015a6bf8423
Franco <franco.perdomo@autoscertificados.com.ar>
2022-12-18 02:56:20 -0300

    fix: solved responsiveness issues

--- src/components/Technologies/TechCard.tsx
+++ src/components/Technologies/TechCard.tsx
@@ -7,19 +7,19 @@ const TechCard : React.FC<TechCardProps> = (props : TechCardProps) => {
         <article className="mx-auto mb-8 items-center justify-evenly rounded-lg border-2 py-8 font-author dark:bg-white dark:text-[#111] w-full">
             <div className="mx-auto w-11/12">
                 <div className="mx-auto mb-6 w-11/12 text-center xl:w-8/12">
-                    <h1 className="mb-1 text-5xl font-medium opacity-80 py-4 lg:text-3xl lg:py-0">
+                    <h1 className="mb-1 text-3xl font-medium opacity-80">
                         {title}
                     </h1>
-                    <h2 className="text-3xl font-medium leading-snug opacity-60 pb-4 lg:text-lg lg:pb-0">
+                    <h2 className="text-md font-medium leading-snug opacity-60 lg:text-lg">
                         {description}
                     </h2>
-                    <div className="flex">
+                    <div className="mx-auto mt-4 flex justify-center">
                     {techs?.map((tech, index) => {
                         return(
                         <div className="mx-auto mt-4 flex justify-center" key={index}>
-                            <div className="mx-3 flex h-32 w-32 flex-col items-center justify-center lg:w-12 lg:h-12">
-                                <img src={tech.image} className="w-32 h-32 lg:w-14 lg:h-14" loading="lazy" style={{color: "transparent"}} />
-                                <p className="font-medium capitalize opacity-90">
+                            <div className=" mx-3 flex h-[45px] w-[45px] flex-col items-center justify-center">
+                                <img src={tech.image} width="50" height="50" decoding="async" data-nimg="1" className="max-h-[45px] min-h-[45px] min-w-[45px] max-w-[45px]" loading="lazy" style={{color: "transparent"}} />
+                                <p className="font-medium capitalize opacity-90 text-xs lg:text-base">
                                     {tech.name}
                                 </p>
                             </div>
--- src/components/Technologies/TechSection.tsx
+++ src/components/Technologies/TechSection.tsx
@@ -4,7 +4,7 @@ import TechCard from "./TechCard";
 export default function TechSection() {
     return(
         <section className="mx-auto mb-4 w-11/12 max-w-[90rem] text-center"> 
-            <h1 className="text-white mb-1 text-center font-author text-6xl font-medium opacity-80 dark:opacity-100 lg:text-4xl">
+            <h1 className="text-white mb-1 text-center font-author text-3xl font-medium opacity-80 dark:opacity-100 lg:text-4xl">
                 Technologies
             </h1>
             <div className="mx-auto mt-10 grid max-w-[90rem] grid-cols-1 gap-4 lg:grid-cols-2">
@@ -12,19 +12,19 @@ export default function TechSection() {
                     title="Core"
                     description="The core technologies I utilise everyday + Typescript for type safety."
                     techs={[
-                        {name: "HTML5", image: "https://img.icons8.com/color/64/000000/html-5--v1.png"},
-                        {name: "CSS3", image: "https://img.icons8.com/color/64/000000/css3.png"},
-                        {name: "JavaScript", image: "https://img.icons8.com/color/64/000000/javascript--v1.png"},
-                        {name: "TypeScript", image: "https://img.icons8.com/color/64/000000/typescript.png"},
+                        {name: "HTML5", image: "https://img.icons8.com/color/48/000000/html-5--v1.png"},
+                        {name: "CSS3", image: "https://img.icons8.com/color/48/000000/css3.png"},
+                        {name: "JavaScript", image: "https://img.icons8.com/color/48/000000/javascript--v1.png"},
+                        {name: "TypeScript", image: "https://img.icons8.com/color/48/000000/typescript.png"},
                     ]}
                 />
                 <TechCard 
                     title="Frontend"
                     description="The technologies I use when making frontend apps."
                     techs={[
-                        {name: "React", image: "https://img.icons8.com/color/64/000000/react-native.png"},
-                        {name: "Next.js", image: "https://img.icons8.com/color/64/nextjs.png"},
-                        {name: "Tailwind CSS", image: "https://img.icons8.com/color/64/tailwindcss.png"},
+                        {name: "React", image: "https://img.icons8.com/color/48/000000/react-native.png"},
+                        {name: "Next.js", image: "https://img.icons8.com/color/48/nextjs.png"},
+                        {name: "Tailwind CSS", image: "https://img.icons8.com/color/48/tailwindcss.png"},
                     ]}
                 />
             </div>
